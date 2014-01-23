b6479c20f1707fa6580daafa80f83ccffa912176
Jan Jongboom <janjongboom@gmail.com>
2014-01-23 13:34:08 +0100

    Offline

---
+++ offline-finished/currency.js
@@ -0,0 +1,58 @@
+var _rate;
+
+var result = document.querySelector('#result span');
+var input = document.querySelector('#nok');
+var refresh = document.querySelector('#refresh');
+
+function getExchangeRate() {
+  var x = new XMLHttpRequest();
+  x.onload = function() {
+    if (x.status === 200) {
+      _rate = Number(JSON.parse(x.responseText).query.results.rate.Rate);
+      
+      localStorage.setItem('rate', _rate);
+    }
+    else {
+      _rate = Number(localStorage.getItem('rate'));
+    }
+  };
+  
+  x.onerror = function(e) {
+    _rate = Number(localStorage.getItem('rate'));
+  };
+
+  x.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22NOKEUR%22%29&env=store://datatables.org/alltableswithkeys&format=json');
+  x.send();
+}
+
+// On page load, get the exchange rate
+getExchangeRate();
+
+// Clicking refresh should also get exchange rate
+refresh.addEventListener('click', function() {
+  getExchangeRate();
+});
+
+// When you type we'll update the Amount in EUR
+input.addEventListener('keyup', function() {
+  // No rate available? Then cancel out.
+  if (!_rate) {
+    result.textContent = 'No rate available';
+    return;
+  }
+
+  // Grab the value from the user, and if it's a number show result
+  var value = Number(input.value);
+  if (isNaN(value)) return;
+  
+  result.textContent = '€ ' + (value * _rate).toFixed(2);
+});
+
+
+window.applicationCache.addEventListener('updateready', function(e) {
+  // Browser downloaded a new app cache.
+  if (confirm('A new version of this site is available. Load it?')) {
+    window.location.reload();
+  }
+});
+
---
+++ offline/currency.js
@@ -0,0 +1,51 @@
+var _rate;
+
+var result = document.querySelector('#result span');
+var input = document.querySelector('#nok');
+var refresh = document.querySelector('#refresh');
+
+function getExchangeRate() {
+  var x = new XMLHttpRequest();
+  x.onload = function() {
+    if (x.status === 200) {
+      _rate = Number(JSON.parse(x.responseText).query.results.rate.Rate);
+      
+      localStorage.setItem('rate', _rate);
+    }
+    else {
+      alert('Call to exchange rate server failed ' + x.status);
+      _rate = null;
+    }
+  };
+  
+  x.onerror = function(e) {
+    alert('Error getting exchange rate!');
+    _rate = null;
+  };
+
+  x.open('GET', 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20%28%22NOKEUR%22%29&env=store://datatables.org/alltableswithkeys&format=json');
+  x.send();
+}
+
+// On page load, get the exchange rate
+getExchangeRate();
+
+// Clicking refresh should also get exchange rate
+refresh.addEventListener('click', function() {
+  getExchangeRate();
+});
+
+// When you type we'll update the Amount in EUR
+input.addEventListener('keyup', function() {
+  // No rate available? Then cancel out.
+  if (!_rate) {
+    result.textContent = 'No rate available';
+    return;
+  }
+
+  // Grab the value from the user, and if it's a number show result
+  var value = Number(input.value);
+  if (isNaN(value)) return;
+  
+  result.textContent = '€ ' + (value * _rate).toFixed(2);
+});
